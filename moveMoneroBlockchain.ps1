bb09341b1729eefa041beb667f6c711d20c7a479
KrakenFuego <31135312+KrakenFuego@users.noreply.github.com>
2018-06-18 09:53:59 +0100

    Removed Deprecated BitsTransfer Module

--- moveMoneroBlockchain.ps1
+++ moveMoneroBlockchain.ps1
@@ -1,7 +1,6 @@
 ﻿# Move Monero Blockchain Location
 # Ⓒ 2018 @Krakenfuego
 # MIT License
-Import-Module BitsTransfer
 
 # Elevate Admin Permissions
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs}
